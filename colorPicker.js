44dc86fa4eacd85d4bcaf628b15e46bd18fa7b8c
pepyn <renan.rodrigues2026@gmail.com>
2024-10-10 10:58:20 -0300

    Changes to be committed: modified:   colorPicker.js modified:   main.html modified:   src/style.css

--- colorPicker.js
+++ colorPicker.js
@@ -1,11 +1,49 @@
 const c = document.getElementById("matriz");
 const ctx = c.getContext("2d");
 
-// Create gradient
-const grad=ctx.createRadialGradient(150,75,15,150,75,150);
-grad.addColorStop(0,"lightblue");
-grad.addColorStop(1,"darkblue");
+const l = document.getElementById("line");
+const ltx = l.getContext("2d");
 
-// Fill rectangle with gradient
+const select = document.getElementById('selecao');
+
+// Create gradient
+const grad=ctx.createLinearGradient(0,0,c.width,0);
+grad.addColorStop(0,"white");
+grad.addColorStop(1,"red");
+
 ctx.fillStyle = grad;
-ctx.fillRect(10,10,280,130);
+ctx.fillRect(0,0,c.width,c.height);
+
+const gradB = ctx.createLinearGradient(0,0,0,c.height);
+gradB.addColorStop(0,'rgba(255,255,255,0');
+gradB.addColorStop(0.5,'rgba(255,255,255,0');
+gradB.addColorStop(0.5,'rgba(0,0,0,0')
+gradB.addColorStop(1,'rgba(0,0,0,1');
+
+ctx.fillStyle = gradB;
+ctx.fillRect(0,0,c.width,c.height);
+
+
+const linha = ltx.createLinearGradient(0,0,l.width,0);
+linha.addColorStop(0,"rgb(255,0,0)");
+linha.addColorStop(0.15,"rgb(255,0,255)");
+linha.addColorStop(0.32,"rgb(0,0,255)");
+linha.addColorStop(0.46,"rgb(0,255,255)");
+linha.addColorStop(0.63,"rgb(0,255,0)");
+linha.addColorStop(0.78,"rgb(255,255,0)");
+linha.addColorStop(1,"red");
+
+ltx.fillStyle = linha;
+ltx.fillRect(0,0,l.width,l.height);
+
+
+        // Evento para capturar a cor na posição do clique
+        c.addEventListener('click', (event) => {
+            const x = event.offsetX;
+            const y = event.offsetY;
+
+            const pixel = ctx.getImageData(x, y, 1, 1).data;
+            const color = `rgb(${pixel[0]}, ${pixel[1]}, ${pixel[2]})`;
+
+            select.style.background = color
+        });
